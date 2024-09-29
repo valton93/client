54bd945e0f161b900978aee4434953b60ce2cbb6
valton93 <69967070+valton93@users.noreply.github.com>
2024-09-29 09:42:40 +0530

    Add files via upload

---
+++ src/test/java/com/hari/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
